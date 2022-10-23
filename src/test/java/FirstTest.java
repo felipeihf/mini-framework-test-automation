0c6df24202295a17868de3f0fc5cb5aad2022ac9
Felipe Herrera <tempestpgo@gmail.com>
2022-10-23 00:36:57 -0400

    initial framework setup

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,19 @@
+import org.testng.annotations.Test;
+import pages.BasePage;
+import utils.BaseTest;
+
+public class FirstTest extends BaseTest {
+
+    @Test
+
+    public void homePageTest() {
+
+        homePage = new BasePage();
+
+
+
+
+
+    }
+
+}
---
+++ src/test/java/utils/BaseTest.java
@@ -0,0 +1,41 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import pages.BasePage;
+
+
+public class BaseTest {
+
+
+    private static WebDriver webdriver;
+    protected static BasePage homePage;
+
+    @BeforeClass
+    public static void setupDriver() {
+
+        WebDriverManager.chromedriver().setup();
+
+    }
+
+    @BeforeMethod
+    public static void launchBrowser() {
+
+        webdriver = new ChromeDriver();
+        webdriver.get(Links.HOMEPAGE);
+
+    }
+
+    @AfterClass
+    public static void closeBrowser() {
+        webdriver.quit();
+
+    }
+
+
+
+}
